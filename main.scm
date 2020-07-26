82a7561bb47bafb947ce78e3af09e46e016c6457
Dina Bogdan <bogdan.dina03@gmail.com>
2020-07-26 13:19:59 +0000

    define procedures and compound procedures

--- main.scm
+++ main.scm
@@ -12,3 +12,9 @@
 (define pi 3.14159)
 (define radius 10)
 (define circumference (* 2 pi radius))
+
+;; procedure definitions
+
+(define (square x) (* x x))
+
+(define (sum-of-squares x y) (+ (square x) (square y)))

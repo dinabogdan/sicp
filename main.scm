9df6cd892b583191026ae519e095a721b1ce671f
dinabogdan <bogdan.dina03@gmail.com>
2020-07-28 16:29:45 +0300

    add fermat test

--- main.scm
+++ main.scm
@@ -215,3 +215,86 @@
         ((= column 1) 1)
         ((+ (pascal-triangle (- row 1) (- column 1))
             (pascal-triangle (- row 1) column)))))
+
+;; exponentiation
+
+;; recursive implementation
+
+(define (expt b n)
+  (if (= n 0)
+    1
+    (* b (expt b (- n 1)))))
+
+;; iterative / tail-recursive implementation
+
+(define (tailrec-expt b n)
+  (expt-iter b n 1))
+(define (expt-iter b counter product)
+  (if (= counter 0)
+      product
+      (expt-iter b (- counter 1) (* b product))))
+
+;; logarithmically growth implementation
+
+(define (fast-expt b n)
+  (cond ((= n 0) 1)
+        ((even? n) (square (fast-expt b (/ n 2))))
+        (else (* b (fast-expt b (- n 1))))))
+
+(define (even? n)
+  (= (mod n 2) 0))
+
+;; exercise 1.16
+
+(define (iter-fast-expt b n)
+  (define (iter n b acc)
+    (cond ((= 0 n) acc)
+          ((even? n) (iter (/ n 2) (square b) acc))
+          (else (iter (- n 1) b (* b acc)))))
+  (iter n b 1))
+
+;; GCD algorithm / Euclid's Algorithm
+
+(define (gcd a b)
+  (if (= b 0)
+      a
+      (gcd b (mod a b))))
+
+;; searching for divisors of n
+;; one way to find if n is prime or not
+
+(define (smallest-divisor n) (find-divisor n 2))
+
+(define (find-divisor n test-divisor)
+  (cond ((> (square test-divisor) n) n)
+        ((divides? test-divisor n) test-divisor)
+        (else (find-divisor n (+ test-divisor 1)))))
+
+(define (divides? a b) (= (mod b a) 0))
+
+;; n is prime iff n is its own smallest divisor
+
+(define (prime? n) (= n (smallest-divisor n)))
+
+;; fermat test for finding if a number is prime
+
+
+;; this one computes the exponential of a number modulo another number
+;; in fact computes (mod base^exp m)
+;; (expmod 2 8 2) = 0, (expmod 2 8 3) = 1
+
+(define (expmod base exp m)
+  (cond ((= exp 0) 1)
+        ((even? exp)
+          (mod 
+            (square (expmod base (/ exp 2) m)) m))
+        (else 
+          (mod 
+            (* base (expmod base (- exp 1) m)) m))))
+
+;; this is fermat test for checking if a number is prime using Fermat's theorem
+
+(define (fermat-test n)
+  (define (try-it a)
+    (= (expmod a n n) a))
+  (try-it (+ 1 (random-integer n))))

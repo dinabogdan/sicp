d9af8f992b6d2b0efdd352b4cc7674dd39659751
dinabogdan <bogdan.dina03@gmail.com>
2020-07-29 12:08:40 +0300

    higher order functions for defining multiple sums

--- main.scm
+++ main.scm
@@ -353,4 +353,45 @@
         ((divides? test-divisor n) test-divisor)
         (else (find-divisor n (next test-divisor)))))
 
+;; 1.3.1 Higher Order Procedures - Procedures as Arguments
 
+(define (cube a)
+  (* a a a))
+
+(define (sum-of-integers a b)
+  (if (> a b)
+      0
+      (+ a (sum-of-integers (+ a 1) b))))
+
+(define (sum-of-cubes a b)
+  (if (> a b)
+      0
+      (+ (cube a)
+         (sum-of-cubes (+ a 1) b))))
+
+(define (pi-sum a b)
+  (if (> a b)
+      0
+      (+ (/ 1.0 (* a (+ a 2)))
+          (pi-sum (+ a 4) b))))
+
+(define (sum term a next b)
+  (if (> a b)
+      0
+      (+ (term a)
+         (sum term (next a) next b))))
+
+(define (inc n) (+ n 1))
+(define (ho-sum-cubes a b)
+  (sum cube a inc b))
+
+(define (identity x) x)
+(define (ho-sum-of-integers a b)
+  (sum identity a inc b))
+
+(define (ho-pi-sum a b)
+  (define (pi-term x)
+    (/ 1.0 (* x (+ x 2))))
+  (define (pi-next x)
+    (+ x 4))
+  (sum pi-term a pi-next b))

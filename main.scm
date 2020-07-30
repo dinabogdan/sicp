e465b0ef600d60d097d2d5c09110d511fe6de4e1
dinabogdan <bogdan.dina03@gmail.com>
2020-07-30 09:53:29 +0300

    add accumulate function

--- main.scm
+++ main.scm
@@ -453,3 +453,27 @@
       result
       (iter (next a) (* result (term a)))))
   (iter a 1))
+
+;; exercise 1.32 - abstract even more through accumulate function
+
+
+;; recursive implementation
+
+(define (accumulate combiner null-value term a next b)
+  (if (> a b)
+    null-value
+    (combiner 
+      (term a) 
+      (accumulate combiner null-value term (next a) next b))))
+
+;; iterative implementation
+
+(define  (iter-accumulate combiner null-value term a next b)
+  (define (iter a result)
+    (if (> a b)
+      result
+      (iter (next a) (combiner result (term a)))))
+  (iter a null-value))
+
+
+  
